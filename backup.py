1247df613b8a3b314447bb9ade59beca3df276b2
Maxx Boehme <maxxboehme@gmail.com>
2014-10-23 22:22:46 -0500

    Added Generating test and copying files

--- backup.py
+++ backup.py
@@ -31,8 +31,28 @@ def _file_copy(source_folder, destination_folder):
          dst_path = os.path.join(destination_folder, src_path.replace(source_folder, ""))
          if not os.path.exists(dst_path):
             os.mkdir(dst_path)
+
+def _my_file_copy(source, destination):
+   copyCount = 0
+   totalNumberOfFiles = _file_count(source)
+   fileCount = 0
+   for root, dirs, files in os.walk(source):
+      for file in files:
+         fileCount += 1
+         if not os.path.exists(os.path.dirname(destination+root.strip(".")+"\\"+file)):
+            os.makedirs(os.path.dirname(destination+root.strip(".")+"\\"+file))
+         if not os.path.exists(destination+root.strip(".")+"\\"+file):
+            shutil.copy(root+"\\"+file, destination+root.strip(".")+"\\"+file)
+            copyCount += 1
+         else:
+            if os.stat(root+"\\"+file).st_mtime > os.stat(destination+root.strip(".")+"\\"+file).st_mtime:
+               shutil.copy(root+"\\"+file, destination+root.strip(".")+"\\"+file)
+               copyCount += 1
+         _print_loading_bar2(fileCount, totalNumberOfFiles, 50)
+         time.sleep(1)
+            
    
-def _print_loading_bar(x, newline, maxLimit, numberBars):
+def _print_loading_bar(x, maxLimit, numberBars):
    percentag = (x*100) / maxLimit
    #print "percentage: " + str(percentag)
    colums = 100.0 / numberBars
@@ -41,10 +61,27 @@ def _print_loading_bar(x, newline, maxLimit, numberBars):
    #print "bars: " + str(bars)
    arrow = ">" if (bars != numberBars) else ""
    print("[" + ("=" * bars) + arrow + (" " * (numberBars - bars - 1))+"]")
-   if not newline:
+   if x < maxLimit:
       sys.stdout.write("\033[F") # Cursor up one line
+      
+def _gen_files(destination, numberOfDirs, numberOfFiles, maxHeight):
+   _gen_files_rec(destination, numberOfDirs, numberOfFiles, 0, maxHeight)
+      
+def _gen_files_rec(path, numberOfDirs, numberOfFiles, height, maxHeight):
+   if height == maxHeight:
+      return
+   else:
+      for i in xrange(0, numberOfDirs):
+         newPath = path+"\\dev"+str(i)+"_"+str(height)
+         print(newPath)
+         os.makedirs(newPath)
+         _gen_files_rec(newPath, numberOfDirs, numberOfFiles, height+1, maxHeight)
+      for i in xrange(0, numberOfFiles):
+         open(path+"\\TestFile"+str(i)+".txt", "w+")
+      
+         
 
-def _print_loading_bar2(x, newline, maxLimit, numberBars):
+def _print_loading_bar2(x, maxLimit, numberBars):
    percentag = (x*100) / maxLimit
    #print "percentage: " + str(percentag)
    colums = 100.0 / numberBars
@@ -52,14 +89,15 @@ def _print_loading_bar2(x, newline, maxLimit, numberBars):
    bars = int(percentag / colums)
    #print "bars: " + str(bars)
    arrow = ">" if (bars != numberBars) else ""
-   end = '\n' if newline else '\r'
+   end = '\n' if x >= maxLimit else '\r'
    print("[" + ("=" * bars) + arrow + (" " * (numberBars - bars - 1))+"]", end=end)
    sys.stdout.flush()
 
 def _main(args):
    print(_file_count(".\\test"))
    #shutil.copytree(".\\test", ".\\testTemp")
-   _file_copy(".\\test", ".\\testTemp")
+   #_my_file_copy(".\\test", ".\\testTemp")
+   _gen_files(".\\testGen", 5, 5, 3)
    '''
    for i in xrange(0, MAX+1):
       newline = True if i == MAX else False
