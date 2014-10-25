6fe0ab61b6c4533f3194e8b9d84837a0819aeecf
Maxx Boehme <maxxboehme@gmail.com>
2014-10-24 21:27:16 -0500

    Added some commands

--- backup.py
+++ backup.py
@@ -5,10 +5,11 @@ import os
 import random
 import time
 import shutil
+import json
 
 BARS = 50
 MAX = 1000
-LOCATION = ".\temp"
+LOCATION = ".\\temp"
 
 def _file_count(folder):
    total = 0
@@ -32,7 +33,7 @@ def _file_copy(source_folder, destination_folder):
          if not os.path.exists(dst_path):
             os.mkdir(dst_path)
 
-def _my_file_copy(source, destination):
+def _my_file_copy(source, destination, forceCopy=False):
    copyCount = 0
    totalNumberOfFiles = _file_count(source)
    fileCount = 0
@@ -41,7 +42,7 @@ def _my_file_copy(source, destination):
          fileCount += 1
          if not os.path.exists(os.path.dirname(destination+root.strip(".")+"\\"+file)):
             os.makedirs(os.path.dirname(destination+root.strip(".")+"\\"+file))
-         if not os.path.exists(destination+root.strip(".")+"\\"+file):
+         if forceCopy or not os.path.exists(destination+root.strip(".")+"\\"+file):
             shutil.copy(root+"\\"+file, destination+root.strip(".")+"\\"+file)
             copyCount += 1
          else:
@@ -49,7 +50,6 @@ def _my_file_copy(source, destination):
                shutil.copy(root+"\\"+file, destination+root.strip(".")+"\\"+file)
                copyCount += 1
          _print_loading_bar2(fileCount, totalNumberOfFiles, 50)
-         time.sleep(1)
             
    
 def _print_loading_bar(x, maxLimit, numberBars):
@@ -73,7 +73,6 @@ def _gen_files_rec(path, numberOfDirs, numberOfFiles, height, maxHeight):
    else:
       for i in xrange(0, numberOfDirs):
          newPath = path+"\\dev"+str(i)+"_"+str(height)
-         print(newPath)
          os.makedirs(newPath)
          _gen_files_rec(newPath, numberOfDirs, numberOfFiles, height+1, maxHeight)
       for i in xrange(0, numberOfFiles):
@@ -90,14 +89,34 @@ def _print_loading_bar2(x, maxLimit, numberBars):
    #print "bars: " + str(bars)
    arrow = ">" if (bars != numberBars) else ""
    end = '\n' if x >= maxLimit else '\r'
-   print("[" + ("=" * bars) + arrow + (" " * (numberBars - bars - 1))+"]", end=end)
+   print("[" + ("=" * bars) + arrow + (" " * (numberBars - bars - 1))+"]"+"\t"+str(x)+"/"+str(maxLimit)+" Files Copied", end=end)
    sys.stdout.flush()
+   
+'''
+backup list keys
+'''
+def _print_keys(keyFiles):
+   for key in keyFiles:
+      print("\t"+key)
+      
+def _write_dictionary_to_file(dictionary, destination):
+   with open(destination, 'w') as outputFile:
+      json.dump(dictionary, destination)
+      
+def _load_dictionary_from_file(dictionary, source):
+   with open(source, 'r') as inputFile:
+      dictonary = json.load(inputFile)
 
 def _main(args):
-   print(_file_count(".\\test"))
+   # print(_file_count(".\\test"))
    #shutil.copytree(".\\test", ".\\testTemp")
-   #_my_file_copy(".\\test", ".\\testTemp")
-   _gen_files(".\\testGen", 5, 5, 3)
+   #_my_file_copy(".\\testGen", ".\\testTemp")
+   #_gen_files(".\\testGen", 2, 20, 5)
+   fileKeys = {}
+   fileKeys["programs"] = (".\\testGen", ".\\testTemp")
+   (source, destination) = fileKeys["programs"]
+   _my_file_copy(source, destination)
+   _print_keys(fileKeys)
    '''
    for i in xrange(0, MAX+1):
       newline = True if i == MAX else False
